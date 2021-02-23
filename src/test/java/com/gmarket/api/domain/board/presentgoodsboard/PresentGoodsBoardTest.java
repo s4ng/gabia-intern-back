954a0acd61b470f9317b222875ae55abc95464be
gm2202969 <lsg@gabia.com>
2021-02-23 17:37:37 +0900

    fix test

--- src/test/java/com/gmarket/api/domain/board/presentgoodsboard/PresentGoodsBoardTest.java
+++ src/test/java/com/gmarket/api/domain/board/presentgoodsboard/PresentGoodsBoardTest.java
@@ -3,8 +3,7 @@ package com.gmarket.api.domain.board.presentgoodsboard;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,14 +12,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 @Transactional
 class PresentGoodsBoardTest {
 
     @Autowired PresentGoodsBoardRepository presentGoodsBoardRepository;
+
+    private Long insertedDataId;
 
     Map<String, Object> createRequestForm() {
 
@@ -41,47 +41,79 @@ class PresentGoodsBoardTest {
         return requestForm;
     }
 
+    @BeforeEach
+    void insertData(TestInfo testInfo) {
+        if(testInfo.getTags().contains("SkipCleanup")) {
+            return;
+        }
+
+        Response res = RestAssured.given()
+                .contentType("application/json")
+                .body(createRequestForm())
+                .when()
+                .post("/boards/share/posts")
+                .then()
+                .extract().response();
+
+        JsonPath jsonPath = res.jsonPath();
+        insertedDataId = ((Number)jsonPath.get("data.boardId")).longValue();
+    }
+
+
+    @AfterEach
+    void deleteData(TestInfo testInfo) {
+        if(testInfo.getTags().contains("SkipCleanup")) {
+            return;
+        }
+
+        if(insertedDataId != null) {
+            presentGoodsBoardRepository.deleteById(insertedDataId);
+        }
+    }
+
     @Test
-    @Order(1)
-    void 나눔글_생성_확인_수정_삭제() {
+    @Tag("SkipCleanup")
+    void 나눔글_생성() {
 
         // 생성 테스트
         Map<String, Object> requestForm = createRequestForm();
 
         Response res = RestAssured.given()
-                    .contentType("application/json")
-                    .body(requestForm).log().all()
+                .contentType("application/json")
+                .body(requestForm).log().all()
                 .when()
-                    .post("/boards/share/posts")
+                .post("/boards/share/posts")
                 .then()
-                    .statusCode(200)
-                    .assertThat().body("data.title", equalTo("test title"))
-                    .assertThat().body("data.userId", equalTo(1))
-                    .log().all()
-                    .extract().response();
+                .statusCode(200)
+                .assertThat().body("data.title", equalTo("test title"))
+                .assertThat().body("data.userId", equalTo(1))
+                .log().all()
+                .extract().response();
 
-        JsonPath jsonPath = res.jsonPath();
-        int boardId = jsonPath.get("data.boardId");
+    }
 
-
+    @Test
+    void 나눔글_조회() {
         // id 확인 테스트
         RestAssured.given()
-                .pathParam("id", boardId)
+                .pathParam("id", insertedDataId)
                 .when()
                 .get("/boards/share/posts/{id}")
                 .then()
                 .statusCode(200)
-                .assertThat().body("data.boardId", equalTo(boardId))
+                .assertThat().body("data.boardId", equalTo((int)(long)insertedDataId))
                 .log().all();
+    }
 
-
+    @Test
+    void 나눔글_수정() {
         // 수정 테스트
         Map<String, Object> map = createRequestForm();
         map.put("title", "change title");
 
         RestAssured.given()
                 .contentType("application/json")
-                .pathParam("id", boardId)
+                .pathParam("id", insertedDataId)
                 .body(map).log().all()
                 .when()
                 .put("/boards/share/posts/{id}")
@@ -89,35 +121,31 @@ class PresentGoodsBoardTest {
                 .statusCode(200)
                 .assertThat().body("data.title", equalTo("change title"))
                 .log().all();
+    }
 
-
+    @Test
+    void 나눔글_삭제() {
         // 삭제 테스트
         RestAssured.given()
-                .pathParam("id", boardId)
+                .pathParam("id", insertedDataId)
                 .when()
                 .delete("/boards/share/posts/{id}")
                 .then()
                 .statusCode(204)
                 .log().all();
-
-
-        // DB 삭제
-        presentGoodsBoardRepository.deleteById((long) boardId);
     }
 
-
     @Test
-    @Order(2)
     void 나눔글_페이지_확인() {
 
         RestAssured.given()
-                        .queryParam("page", "1")
-                    .when()
-                        .get("/boards/share/posts")
-                    .then()
-                        .statusCode(200)
-                        .assertThat().body("data", instanceOf(List.class))
-                        .log().all();
+                .queryParam("page", "1")
+                .when()
+                .get("/boards/share/posts")
+                .then()
+                .statusCode(200)
+                .assertThat().body("data", instanceOf(List.class))
+                .log().all();
     }
 
 }
