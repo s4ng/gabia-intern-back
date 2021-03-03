73287cc5a959db21466960661e8922ae7bdc514e
pablo <kcm@gabia.com>
2021-03-03 19:03:02 +0900

    UserRepository 수정 - Spring Data JPA를 상속 받아 기본 CRUD를 생략하며 로그인 기능에 맞춰 아이디 패스워드로 조회 메서드, 로그인 아이디 조회 메서드 추가

--- src/main/java/com/gmarket/api/domain/user/UserRepository.java
+++ src/main/java/com/gmarket/api/domain/user/UserRepository.java
@@ -1,8 +1,14 @@
 package com.gmarket.api.domain.user;
 
+import com.gmarket.api.domain.user.enums.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository<T extends User> extends JpaRepository<T, Long> {
+
+    public User findByLoginIdAndStatus(String loginId, UserStatus userStatus);
+
+    public User findByLoginIdAndPassword(String loginId, String password);
+
 }
