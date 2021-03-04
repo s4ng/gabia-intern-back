753f9e6458eeb00e35e1ab9b59b9f41234db3f09
gm2202969 <lsg@gabia.com>
2021-03-04 11:36:50 +0900

    update raffle mapper

--- src/main/java/com/gmarket/api/domain/raffle/dto/RaffleMapper.java
+++ src/main/java/com/gmarket/api/domain/raffle/dto/RaffleMapper.java
@@ -2,17 +2,19 @@ package com.gmarket.api.domain.raffle.dto;
 
 import com.gmarket.api.domain.raffle.Raffle;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface RaffleMapper {
 
     RaffleMapper INSTANCE = Mappers.getMapper(RaffleMapper.class);
-    default RaffleResponseDto entityToDto(Raffle raffle) {
-        return RaffleResponseDto.builder()
-                .id(raffle.getRaffleId())
-                .presentBoardId(raffle.getPresentBoard().getBoardId())
-                .userId(raffle.getParticipant().getUserId())
-                .build();
-    }
+
+    @Mappings({
+            @Mapping(target = "id", source = "raffleId"),
+            @Mapping(target = "presentBoardId", source = "presentBoard.boardId"),
+            @Mapping(target = "userId", source = "participant.userId")
+    })
+    RaffleResponseDto entityToDto(Raffle raffle);
 }
