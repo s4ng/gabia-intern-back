42c9e836a944711ac40ecb3f26537b10e8722d0e
pablo <kcm@gabia.com>
2021-03-08 09:51:05 +0900

    BoardRepository 수정 - subType 엔티티 조회를 위해 EntityManager을 활용할  BoardRepository 수정

--- src/main/java/com/gmarket/api/domain/board/BoardRepository.java
+++ src/main/java/com/gmarket/api/domain/board/BoardRepository.java
@@ -6,36 +6,25 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository // Bean Component
-@RequiredArgsConstructor // final 선언한 변수를 매개 변수로 받는 생성자 만듬, @NonNull 필드 값만 파라미터로 받는 생성자를 만듬
+@Repository
+@RequiredArgsConstructor // final -> 생성자
 public class BoardRepository {
 
     private final EntityManager em;
 
-    public List<Board> findBoardPage(String sql, int pageNumber) {
-        return em.createQuery(sql, Board.class)
-                .setFirstResult((pageNumber-1)*10)
-                .setMaxResults(10)
-                .getResultList();
+    // 다형성 구현을 위해서는 JPA 쿼리가 Board 엔티티가 아닌, Board SubType 엔티티를 조회해야 합니다.
+    // 또한, 가급적 쿼리를 사용하지 않고 JPA에서 제공하는 메서드로 Repository를 구현하고자 합니다.
+    // Spring data JPA를 활용한 Repository에서 Board 엔티티가 아닌 Board SubType 엔티티로 조회하는 메서드 구현의 어려움으로 인해
+    // 현재로서는 EntityManager 메서드를 이용하여 처리하였습니다.
+    // 추후에 공부하여 Spring data jpa Repository의 메서드로 Board SubType 엔티티로 조회하도록 변경할 예정입니다.(또는 QueryDsl)
+
+    public Board findById(Board board, Long boardId){
+        return em.find(board.getClass(), boardId);
     }
 
-    public Board findPost(String sql) {
-        Board board = em.createQuery(sql, Board.class).getSingleResult();
-        board.addViewCount();
-        return board;
-    }
-
-    public Board createPost(Board board) {
-        em.persist(board);
-        return board;
-    }
-
-    public Board updatePost(Board board) {
-        return em.find(board.getClass(), board.getBoardId());
-    }
-
-    public Long deletePost(Board board) {
-        em.find(board.getClass(), board.getBoardId());
-        return board.getBoardId();
+    public List<Board> findPage(Board board, int page){
+        return (List<Board>) em.createQuery("SELECT m FROM "+board.getClass().getSimpleName()+
+                " m WHERE m.status != 'DELETED' ORDER BY m.boardId DESC").
+                setFirstResult((page-1)*10).setMaxResults(10).getResultList();
     }
 }
