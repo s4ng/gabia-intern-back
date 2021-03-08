82401558d00820e082e57650275a212549a7cb1d
pablo <kcm@gabia.com>
2021-03-08 16:55:09 +0900

    CommentDto 수정 - Comment 엔티티에 매핑 될 값 추가 및 엔티티 -> dto 맵핑 메서드 추가

--- src/main/java/com/gmarket/api/domain/comment/dto/CommentDto.java
+++ src/main/java/com/gmarket/api/domain/comment/dto/CommentDto.java
@@ -1,21 +1,44 @@
 package com.gmarket.api.domain.comment.dto;
 
 import com.gmarket.api.domain.board.Board;
+import com.gmarket.api.domain.comment.Comment;
+import com.gmarket.api.domain.comment.enums.BoardType;
 import com.gmarket.api.domain.comment.enums.CommentStatus;
 import com.gmarket.api.domain.user.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class CommentDto {
-    private long commentId;
-    private User userId;
-    private Board boardId;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class CommentDto { // 댓글의 경우 Sub Entity Class 내용이 일치하여 Sub Dto Class 생략
+
+    private BoardType boardType;
+
+    private Long commentId;
+
+    private Long boardId;
+
+    private Long userId;
+
     private String comment;
+
     private CommentStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
+    public CommentDto  entityToDto(Comment comment){
+        this.boardType = comment.getBoardType();
+        this.commentId = comment.getCommentId();
+        this.boardId = comment.getBoard().getBoardId();
+        this.userId = comment.getUser().getUserId();
+        this.comment = comment.getComment();
+        this.status = comment.getStatus();
+        this.createdAt = comment.getCreatedAt();
+        this.modifiedAt = comment.getModifiedAt();
+        return this;
+    }
 }
