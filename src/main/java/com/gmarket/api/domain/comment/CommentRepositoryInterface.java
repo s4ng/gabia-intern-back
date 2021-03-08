c4d3903c1cbe8e16c7995f2e0d324bdb8e6ffd68
pablo <kcm@gabia.com>
2021-03-08 16:59:18 +0900

    CommentRepositoryInterface 생성 -  기본 CRUD Spring data jpa 활용

---
+++ src/main/java/com/gmarket/api/domain/comment/CommentRepositoryInterface.java
@@ -0,0 +1,11 @@
+package com.gmarket.api.domain.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepositoryInterface extends JpaRepository<Comment, Long> {
+
+}
