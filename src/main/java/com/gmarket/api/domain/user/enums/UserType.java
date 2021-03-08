a8a940784ebdb725af4453a1a5b56c8e95ecdeb7
pablo <kcm@gabia.com>
2021-03-08 12:34:22 +0900

    UserType 수정 - UserType으로 각 SubType Entity을 매핑하기 위한 메서드 추가

--- src/main/java/com/gmarket/api/domain/user/enums/UserType.java
+++ src/main/java/com/gmarket/api/domain/user/enums/UserType.java
@@ -1,16 +1,36 @@
 package com.gmarket.api.domain.user.enums;
 
+import com.gmarket.api.domain.user.User;
+import com.gmarket.api.domain.user.dto.UserDto;
+import com.gmarket.api.domain.user.subclass.manager.Manager;
+import com.gmarket.api.domain.user.subclass.member.Member;
 import lombok.Getter;
 
 @Getter
 public enum UserType {
     MANAGER, MEMBER;
 
-    private String type;
+    // UserType -> User SubClass
+    public static User userTypeToSubClass(UserType userType){
+        switch (userType){
+            case MEMBER:
+                return new Member();
+            case MANAGER:
+                return new Manager();
+            default:
+                throw new IllegalStateException("정확한 유저 타입을 입력하세요");
+        }
+    }
 
-    public static class Values {
-        public static final String MANAGER = "manager";
-        public static final String MEMBER = "member";
+    // UserType -> UserDto
+    public static UserDto userTypeToSubClassDto(UserType userType){
+        switch (userType){
+            case MEMBER:
+            case MANAGER:
+                return new UserDto();
+            default:
+                throw new IllegalStateException("정확한 유저 타입을 입력하세요");
+        }
     }
 
 }
