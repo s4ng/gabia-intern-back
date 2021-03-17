1ec29141267ec07eb4a7469578eb9617cea1d07e
pablo <kcm@gabia.com>
2021-03-17 15:07:25 +0900

    PriceSuggestionService Update - 가격 제안 요청을 처리하는 서비스 로직에 실시간 가격 제안 알림을 보내주는 로직 추가

--- src/main/java/com/gmarket/api/domain/pricesuggestion/PriceSuggestionService.java
+++ src/main/java/com/gmarket/api/domain/pricesuggestion/PriceSuggestionService.java
@@ -1,15 +1,20 @@
 package com.gmarket.api.domain.pricesuggestion;
 
-
+import com.gmarket.api.domain.alert.Alert;
+import com.gmarket.api.domain.alert.AlertRepository;
+import com.gmarket.api.domain.alert.dto.AlertDto;
+import com.gmarket.api.domain.alert.enums.AlertType;
 import com.gmarket.api.domain.board.Board;
-import com.gmarket.api.domain.board.BoardRepository;
+import com.gmarket.api.domain.board.BoardRepositoryInterface;
+import com.gmarket.api.domain.board.enums.BoardStatus;
 import com.gmarket.api.domain.board.subclass.usedgoodsboard.UsedGoodsBoard;
+import com.gmarket.api.domain.pricesuggestion.dto.PriceSuggestionDto;
 import com.gmarket.api.domain.pricesuggestion.enums.PriceSuggestionStatus;
 import com.gmarket.api.domain.user.User;
-import com.gmarket.api.domain.pricesuggestion.dto.PriceSuggestionDto;
-import com.gmarket.api.domain.user.UserRepositoryInterface;
+import com.gmarket.api.domain.user.UserRepository;
 import com.gmarket.api.domain.user.enums.UserStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,57 +25,75 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class PriceSuggestionService {
 
-    private final PriceSuggestionRepositoryInterface priceSuggestionRepositoryInterface;
+    private final PriceSuggestionRepository priceSuggestionRepository;
 
-    private final UserRepositoryInterface userRepositoryInterface;
+    private final UserRepository userRepositoryInterface;
 
-    private final BoardRepository boardRepository;
+    private final BoardRepositoryInterface boardRepositoryInterface;
+
+    private final AlertRepository alertRepository;
+
+    private final SimpMessagingTemplate messagingTemplate;
 
     @Transactional
-    public PriceSuggestionDto save(PriceSuggestionDto priceSuggestionDto) {
+    public PriceSuggestionDto save(PriceSuggestionDto priceSuggestionDto){
 
         Optional<User> optionalUser = userRepositoryInterface.findById(priceSuggestionDto.getUserId());
 
-        if (optionalUser.isEmpty()) {
+        if(optionalUser.isEmpty()){
             throw new IllegalStateException("유효하지 않은 유저 식별 값 입니다.");
         }
 
-        if (optionalUser.get().getStatus().equals(UserStatus.DELETED)) {
+        if(optionalUser.get().getStatus().equals(UserStatus.DELETED)){
             throw new IllegalStateException("이미 탈퇴한 유저 입니다.");
         }
 
-        Board board = boardRepository.findById(new UsedGoodsBoard(), priceSuggestionDto.getBoardId());
+        Board board = boardRepositoryInterface.findById(priceSuggestionDto.getBoardId())
+                .orElseThrow( () -> new IllegalStateException("유효하지 않은 게시판 식별 값 입니다.") );
 
-        if (board == null) {
-            throw new IllegalStateException("유효하지 않은 게시판 식별 값 입니다.");
+        if(board.getStatus().equals(BoardStatus.CLOSED)
+                || board.getStatus().equals(BoardStatus.DELETED) ){
+            throw new IllegalStateException("이미 판매 완료 또는 삭제된 게시글은 가격 제안이 불가합니다");
         }
 
-        UsedGoodsBoard usedGoodsBoard = (UsedGoodsBoard) board;
-
-        if (usedGoodsBoard.getUser().getUserId().equals(priceSuggestionDto.getUserId())) {
-            throw new IllegalStateException("판매 글의 작성자는 본인이 작성한 판매 글의 가격 제안이 불가합니다.");
+        if(board.getUser().getUserId().equals(priceSuggestionDto.getUserId())){
+            throw new IllegalStateException("본인이 작성한 판매 글의 가격 제안은 불가합니다.");
         }
 
-        if (!usedGoodsBoard.isPriceSuggestion()) {
+        UsedGoodsBoard usedGoodsBoard = (UsedGoodsBoard ) board;
+
+        if(!usedGoodsBoard.isPriceSuggestion()){
             throw new IllegalStateException("가격 제안 불가 상품 입니다.");
         }
 
-        PriceSuggestion priceSuggestion = new PriceSuggestion();
-
-        priceSuggestion.dtoToEntity(priceSuggestionDto);
+        // 가격 제안 생성
+        PriceSuggestion priceSuggestion = new PriceSuggestion().dtoToEntity(priceSuggestionDto);
 
         priceSuggestion.suggestionStatus();
 
-        priceSuggestion.userAndBoardSetting(optionalUser.get(), board);
+        priceSuggestion.userAndBoardSetting(optionalUser.get(), usedGoodsBoard);
 
-        return priceSuggestionDto.entityToDto(priceSuggestionRepositoryInterface.save(priceSuggestion));
+        priceSuggestionRepository.save(priceSuggestion); // 가격 제안 저장
+
+        // 가격 제안 알림 생성
+        String message = "가격제안: "+board.getTitle()+ "\n제시금액: "+ priceSuggestionDto.getSuggestionPrice();
+
+        Alert alert = new Alert().createAlert(board.getUser(), board, message, AlertType.SUGGESTION);
+
+        alertRepository.save(alert); // 가격 제안 알림 저장
+
+        // 가격 제안 알림 웹 소켓 전송
+        messagingTemplate.convertAndSend(
+                "/sub/alert/" + board.getUser().getUserId() , new AlertDto().entityToDto(alert));
+
+        return priceSuggestionDto.entityToDto(priceSuggestion);
     }
 
     @Transactional
     public PriceSuggestionDto update(PriceSuggestionDto priceSuggestionDto){
 
         Optional<PriceSuggestion> optionalPriceSuggestion =
-                priceSuggestionRepositoryInterface.findById(priceSuggestionDto.getPriceSuggestionId());
+                priceSuggestionRepository.findById(priceSuggestionDto.getPriceSuggestionId());
 
         if(optionalPriceSuggestion.isEmpty()){
             throw new IllegalStateException("가격 제안 조회 내역이 존재하지 않습니다.");
@@ -92,15 +115,15 @@ public class PriceSuggestionService {
 
         priceSuggestion.update(priceSuggestionDto.getSuggestionPrice(), priceSuggestionDto.getStatus());
 
-        return priceSuggestionDto.entityToDto(priceSuggestionRepositoryInterface.save(priceSuggestion));
+        return priceSuggestionDto.entityToDto(priceSuggestionRepository.save(priceSuggestion));
     }
 
     @Transactional // 가격 수락의 경우 채팅 방 생성 기능이 함께 트랜잭션
-    // 따라서 채팅 관련 Dto가 리턴되어야 하는지 생각
+                   // 따라서 채팅 관련 Dto가 리턴되어야 하는지 생각
     public PriceSuggestionDto accept(PriceSuggestionDto priceSuggestionDto){
 
         Optional<PriceSuggestion> optionalPriceSuggestion =
-                priceSuggestionRepositoryInterface.findById(priceSuggestionDto.getPriceSuggestionId());
+                priceSuggestionRepository.findById(priceSuggestionDto.getPriceSuggestionId());
 
         if(optionalPriceSuggestion.isEmpty()){
             throw new IllegalStateException("가격 제안 조회 내역이 존재하지 않습니다.");
@@ -122,7 +145,8 @@ public class PriceSuggestionService {
 
         priceSuggestion.acceptStatus();
 
-        return priceSuggestionDto.entityToDto(priceSuggestionRepositoryInterface.save(priceSuggestion));
+        return priceSuggestionDto.entityToDto(priceSuggestionRepository.save(priceSuggestion));
     }
+
 
 }
