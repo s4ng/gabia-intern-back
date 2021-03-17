00d94e8a62e0754530cfafff1b6600780edcb665
pablo <kcm@gabia.com>
2021-03-17 14:29:17 +0900

    AlertRepository Update - Spring data JPA 활용으로 변경하며 비즈니스 로직에 맞는 메서드 추가

--- src/main/java/com/gmarket/api/domain/alert/AlertRepository.java
+++ src/main/java/com/gmarket/api/domain/alert/AlertRepository.java
@@ -1,13 +1,26 @@
 package com.gmarket.api.domain.alert;
 
+import com.gmarket.api.domain.alert.enums.AlertType;
+import com.gmarket.api.domain.board.Board;
+import com.gmarket.api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
 
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Long> {
 
-public class AlertRepository {
+    public List<Alert> findByUserOrderByAlertIdDesc(User user);
 
+    public Alert findTop1ByUserAndAlertTypeAndBoard(User user, AlertType alertType, Board board);
 
+    @Transactional
+    @Modifying
+    @Query("UPDATE Alert m SET m.status = 'READ' WHERE m.status = 'YET' AND m.user = ?1")
+    public void updateStatus(User user);
 
 }
