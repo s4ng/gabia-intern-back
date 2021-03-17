5189bff50b59c72e90725436746e7ffc622841ac
pablo <kcm@gabia.com>
2021-03-17 14:31:17 +0900

    AlertController Update - 프론트 엔드에서 웹소켓 요청에 매핑 될 컨트롤러 메서드 추가

--- src/main/java/com/gmarket/api/domain/alert/AlertController.java
+++ src/main/java/com/gmarket/api/domain/alert/AlertController.java
@@ -1,4 +1,21 @@
 package com.gmarket.api.domain.alert;
 
+import com.gmarket.api.domain.alert.dto.AlertDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class AlertController {
+
+    private final AlertService alertService;
+
+    // 설정한 prefix 포함하면 /pub/alert/user
+    // MessageMapping 실시간으로 알림을 읽었다는 내용을 FrontEnd 측에서 요청할 url 매핑
+    @MessageMapping("/alert/user")
+    public void alertYetList(AlertDto alertDto){
+        alertService.alertYetList(alertDto);
+    }
+
 }
