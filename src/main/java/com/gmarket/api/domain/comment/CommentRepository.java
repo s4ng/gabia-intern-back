dde45b18ed1dd8f28d9300826ff78b7bca672a9d
pablo <kcm@gabia.com>
2021-03-17 14:38:56 +0900

    CommentRepository Update - 해당 글의 댓글 개수를 리턴하는 메서드 추가

--- src/main/java/com/gmarket/api/domain/comment/CommentRepository.java
+++ src/main/java/com/gmarket/api/domain/comment/CommentRepository.java
@@ -3,16 +3,15 @@ package com.gmarket.api.domain.comment;
 import com.gmarket.api.domain.board.Board;
 import com.gmarket.api.domain.comment.enums.CommentStatus;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     public Page<Comment> findByBoardAndStatusNot(Board board, CommentStatus commentStatus, Pageable pageable);
+
+    public int countByBoard(Board board);
 
 }
