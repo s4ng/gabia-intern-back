f76edcdb3dcc2098b7eab694e6e3a56f024d8adb
pablo <kcm@gabia.com>
2021-03-17 15:15:24 +0900

    PriceSuggestionRepositoryInterface Delete - Spring Data JPA를 활용하여 하나의 Repository만 사용하도룍 함

--- src/main/java/com/gmarket/api/domain/pricesuggestion/PriceSuggestionRepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.gmarket.api.domain.pricesuggestion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PriceSuggestionRepositoryInterface extends JpaRepository<PriceSuggestion, Long> {
-
-}
