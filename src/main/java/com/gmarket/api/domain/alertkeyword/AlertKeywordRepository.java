dfe007d574fa218d142c314218b16b406edfc4f1
pablo <kcm@gabia.com>
2021-03-17 15:26:19 +0900

    AlertKeywordRepository Update - Spring Data JPA를 활용하여 class에서 Interface로 변경

--- src/main/java/com/gmarket/api/domain/alertkeyword/AlertKeywordRepository.java
+++ src/main/java/com/gmarket/api/domain/alertkeyword/AlertKeywordRepository.java
@@ -1,4 +1,18 @@
 package com.gmarket.api.domain.alertkeyword;
 
-public class AlertKeywordRepository {
+import com.gmarket.api.domain.alertkeyword.enums.AlertKeywordStatus;
+import com.gmarket.api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AlertKeywordRepository extends JpaRepository<AlertKeyword, Long> {
+
+    public List<AlertKeyword> findByUserAndStatus(User user, AlertKeywordStatus alertKeywordStatus);
+
+    public AlertKeyword findByUserAndKeyword(User user, String keyword);
+
+    public List<AlertKeyword> findByStatus(AlertKeywordStatus alertKeywordStatus);
 }
