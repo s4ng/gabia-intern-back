f6a9d0e952dda7c341d5014fac1f123ee264f39a
pablo <kcm@gabia.com>
2021-03-18 11:29:15 +0900

    Raffle Update - 연관 관계 수정

--- src/main/java/com/gmarket/api/domain/raffle/Raffle.java
+++ src/main/java/com/gmarket/api/domain/raffle/Raffle.java
@@ -1,12 +1,14 @@
 package com.gmarket.api.domain.raffle;
 
 import com.gmarket.api.domain.board.Board;
+import com.gmarket.api.domain.raffle.enums.RaffleStatus;
 import com.gmarket.api.domain.user.User;
 import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
+@Setter
 @Getter
 @Builder
 @NoArgsConstructor
@@ -14,28 +16,25 @@ import javax.persistence.*;
 @Inheritance(strategy = InheritanceType.JOINED) // JPA 상속 매핑 전략 (조인 전략)
 public class Raffle {
     @Id @GeneratedValue
-    private long raffleId;
+    private Long raffleId;
 
     @ManyToOne
-    @JoinColumn(name = "present_board")
-    private Board presentBoard;
+    @JoinColumn(name = "board_id")
+    private Board board;
 
     @ManyToOne
-    @JoinColumn(name = "participant")
-    private User participant;
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @Enumerated(EnumType.STRING)
-    private Status status;
+    private RaffleStatus status;
 
-    public enum Status {
-        CREATE, DELETE
-    }
 
     public void delete() {
-        this.status = Status.DELETE;
+        this.status = RaffleStatus.DELETED;
     }
 
     public void reInsert() {
-        this.status = Status.CREATE;
+        this.status = RaffleStatus.CREATED;
     }
 }
