8fbc46e3cec7df056874a33822a2fbf568b26263
pablo <kcm@gabia.com>
2021-03-18 11:30:58 +0900

    RaffleRepository Update - Raffle 엔티티 정보 수정에 따른 Repository 변경과 래플 정보 업데이트 메서드 추가

--- src/main/java/com/gmarket/api/domain/raffle/RaffleRepository.java
+++ src/main/java/com/gmarket/api/domain/raffle/RaffleRepository.java
@@ -1,9 +1,13 @@
 package com.gmarket.api.domain.raffle;
 
 import com.gmarket.api.domain.board.Board;
+import com.gmarket.api.domain.raffle.enums.RaffleStatus;
 import com.gmarket.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +15,18 @@ import java.util.Optional;
 @Repository
 public interface RaffleRepository extends JpaRepository<Raffle, Long> {
 
-    Optional<Raffle> findByPresentBoardAndParticipant(Board presentBoard, User participant);
+    Optional<Raffle> findByBoardAndUser(Board presentBoard, User participant);
 
-    List<Raffle> findAllByPresentBoard(Board presentBoard);
+    List<Raffle> findAllByBoard(Board presentBoard);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Raffle m SET m.status = ?1 WHERE m.board = ?2")
+    public void raffleLose(RaffleStatus raffleStatus, Board board);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Raffle m SET m.status = ?1 WHERE m.board = ?2 AND m.user =?3")
+    public void raffleWin(RaffleStatus raffleStatus, Board board, User user);
+
 }
